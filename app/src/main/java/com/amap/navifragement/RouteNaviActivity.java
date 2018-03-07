8ce775c996fe7d705951a1df3f67a92bb8708493
yiyi.qi <yiyi.qi@alibaba-inc.com>
2018-03-07 16:31:36 +0800

    将语音播报替换为导航内置版本

--- app/src/main/java/com/amap/navifragement/RouteNaviActivity.java
+++ app/src/main/java/com/amap/navifragement/RouteNaviActivity.java
@@ -17,7 +17,6 @@ import com.amap.api.navi.model.AMapServiceAreaInfo;
 import com.amap.api.navi.model.AimLessModeCongestionInfo;
 import com.amap.api.navi.model.AimLessModeStat;
 import com.amap.api.navi.model.NaviInfo;
-import com.amap.navifragement.util.TTSController;
 import com.autonavi.tbt.TrafficFacilityInfo;
 
 /**
@@ -26,7 +25,7 @@ import com.autonavi.tbt.TrafficFacilityInfo;
 public class RouteNaviActivity extends FragmentActivity implements AMapNaviListener {
 
 	AMapNavi mAMapNavi;
-	TTSController mTtsManager;
+
 	Fragment mNaviFragment;
 
 	@Override
@@ -34,12 +33,12 @@ public class RouteNaviActivity extends FragmentActivity implements AMapNaviListe
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_basic_navi);
-		mTtsManager = TTSController.getInstance(getApplicationContext());
-		mTtsManager.init();
+
 
 		mAMapNavi = AMapNavi.getInstance(getApplicationContext());
 		mAMapNavi.addAMapNaviListener(this);
-		mAMapNavi.addAMapNaviListener(mTtsManager);
+		mAMapNavi.setUseInnerVoice(true);
+
 		mAMapNavi.setEmulatorNaviSpeed(60);
 		boolean gps=getIntent().getBooleanExtra("gps", false);
 		if(gps){
@@ -66,7 +65,7 @@ public class RouteNaviActivity extends FragmentActivity implements AMapNaviListe
 	@Override
 	protected void onPause() {
 		super.onPause();
-		mTtsManager.stopSpeaking();
+
 		//
 		//        停止导航之后，会触及底层stop，然后就不会再有回调了，但是讯飞当前还是没有说完的半句话还是会说完
 		//        mAMapNavi.stopNavi();
@@ -77,8 +76,7 @@ public class RouteNaviActivity extends FragmentActivity implements AMapNaviListe
 	protected void onDestroy() {
 		super.onDestroy();
 		mAMapNavi.stopNavi();
-		mAMapNavi.removeAMapNaviListener(mTtsManager);
-		mTtsManager.destroy();
+
 	}
 
 	@Override
